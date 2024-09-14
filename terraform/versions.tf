6b678914ecc6b7eea6b014906cffc453a0b47b43
MyoTintThein <myotintthein1993@gmail.com>
2024-09-14 12:21:04 +0900

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,1 +1,11 @@
+terraform {
+  cloud {
+    organization = "hellocloud-mtt-tfc"
+    ## Required for Terraform Enterprise; Defaults to app.terraform.io for HCP Terraform
+    hostname = "app.terraform.io"
 
+    workspaces {
+      name = "random-string-ws-2"
+    }
+  }
+}
